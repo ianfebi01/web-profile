e646988f55d0235d13ffa94124e3effa863b3f3d
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2026-03-01 20:58:48 +0700

    feat: Configure robots.ts to disallow crawling in non-production environments.

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,14 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+  const isProduction = process.env.NODE_ENV === 'production'
+
   return {
     rules : [
       {
         userAgent : '*',
-        disallow  : [''],
-        allow     : ['/'],
+        disallow  : isProduction ? [] : ['/'],
+        allow     : isProduction ? ['/'] : [],
       },
     ],
     sitemap : `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
