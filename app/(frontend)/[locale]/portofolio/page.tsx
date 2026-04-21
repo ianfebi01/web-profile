4b91830d62158ee897e3ef6dfa3b08d4ac17d144
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2026-04-21 18:30:23 +0700

    fix: refactor metadata generation and improve portfolio data fetching logic

--- app/(frontend)/[locale]/portofolio/page.tsx
+++ app/(frontend)/[locale]/portofolio/page.tsx
@@ -6,72 +6,80 @@ import { isPayloadReady } from '@/lib/is-payload-ready'
 import { getTranslations, setRequestLocale } from 'next-intl/server'
 import { getPayload } from 'payload'
 import configPromise from '@payload-config'
+import { unstable_cache } from 'next/cache'
+import { Project } from '@/payload-types'
 
-export async function generateMetadata( props: Omit<Props, 'children'> ) {
+export async function generateMetadata(props: Omit<Props, 'children'>) {
   const { locale } = await props.params
 
-  setRequestLocale( locale )
+  setRequestLocale(locale)
 
-  const t = await getTranslations( { locale, namespace : 'portofolio' } )
+  const t = await getTranslations({ locale, namespace: 'portofolio' })
 
-  const title = `${t( 'title' )} | Ian Febi Sastrataruna`
-  const desc = t( 'desc' )
+  const title = `${t('title')} | Ian Febi Sastrataruna`
+  const desc = t('desc')
 
   return {
-    title       : title,
-    description : desc,
-    keywords    : 'Frontend developer portofolio',
+    title: title,
+    description: desc,
+    keywords: 'Frontend developer portofolio',
 
-    openGraph : {
-      title       : title,
-      description : desc,
-      siteName    : 'Ian Febi Sastrataruna',
-      type        : 'website',
+    openGraph: {
+      title: title,
+      description: desc,
+      siteName: 'Ian Febi Sastrataruna',
+      type: 'website',
     },
-    twitter : {
-      card        : 'summary',
-      site        : '@ianfebi01',
-      title       : title,
-      description : desc || '',
+    twitter: {
+      card: 'summary',
+      site: '@ianfebi01',
+      title: title,
+      description: desc || '',
     },
   }
 }
 
-export default async function PortofolioPage( props: Omit<Props, 'children'> ) {
-  const { locale } = await props.params
-
-  const t = await getTranslations( { locale, namespace : 'portofolio' } )
-
-  const responseData = { docs: [] as any[] }
-
-  if (isPayloadReady()) {
+export const getPortofolios = unstable_cache(
+  async (locale: 'en' | 'id'): Promise<Project[] | null> => {
     const payload = await getPayload({ config: configPromise })
-    const data = await payload.find({
+    const res = await payload.find({
       collection: 'projects',
       locale: locale as 'en' | 'id',
       sort: '-createdAt',
       depth: 2,
     })
+    if (res.docs.length === 0) return null
+    return res.docs
+  },
+  ['projects'],
+  { tags: ['projects'] },
+)
 
-    responseData.docs = data.docs
+export default async function PortofolioPage(props: Omit<Props, 'children'>) {
+  const { locale } = await props.params
+
+  const t = await getTranslations({ locale, namespace: 'portofolio' })
+
+  const responseData = { docs: [] as any[] }
+
+  if (isPayloadReady()) {
+    const data = await getPortofolios(locale as 'en' | 'id')
+
+    responseData.docs = data || []
   }
 
   return (
     <main>
-      <section id="portofolio"
-        className="h-fit bg-dark"
-      >
+      <section id="portofolio" className="h-fit bg-dark">
         <div className="max-w-7xl mx-auto px-6 lg:px-8 mt-20 sm:mt-20 flex flex-col gap-4">
-          <Header text={t( 'title' )}
-            link={'/'}
-          />
-          {responseData.docs?.length === 0
-            ? <NoDataFound />
-            : <PortofoliosWrapper portofolios={responseData.docs} />
-          }
+          <Header text={t('title')} link={'/'} />
+          {responseData.docs?.length === 0 ? (
+            <NoDataFound />
+          ) : (
+            <PortofoliosWrapper portofolios={responseData.docs} />
+          )}
         </div>
       </section>
     </main>
   )
 }
-
